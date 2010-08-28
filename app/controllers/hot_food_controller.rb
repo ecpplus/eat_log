ca2b5b5c7dc72ca5489bcbb24f237f3ee4abd3ed
chu <chu@sumika.local>
2010-08-29 00:48:42 +0900

    トップに、最近拾ったものを表示するようにした

--- app/controllers/hot_food_controller.rb
+++ app/controllers/hot_food_controller.rb
@@ -1,8 +1,13 @@
 class HotFoodController < ApplicationController
   def index
-    p session[:twitter_id]
-    p session
-    @daily_hot_foods  = DailyHotFood.find(:all)
+    #@daily_hot_foods  = DailyHotFood.find(:all)
     @weekly_hot_foods = WeeklyHotFood.find(:all)
+
+    @recent_tweets = Tweet.find(
+      :all,
+      :include => :user,
+      :order   => 'created_at DESC',
+      :limit   => 20
+    )
   end
 end
