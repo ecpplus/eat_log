02185da35bae3e53de9cb73aa29a4acbc10ec31c
chu <chu@sumika.local>
2010-08-30 10:44:15 +0900

    ブラックリストをもとに、食べ物を消す作業

--- app/models/black_word.rb
+++ app/models/black_word.rb
@@ -1,2 +1,9 @@
 class BlackWord < ActiveRecord::Base
+  class << self
+    def delete_user_food
+      all.each do |black_word| 
+        UserFood.delete_all(["name = ?", black_word.word])
+      end
+    end
+  end
 end
