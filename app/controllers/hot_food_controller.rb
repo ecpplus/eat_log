caf1dc49c1d41f56f8d469fb3885c1b106c992e4
chu <ecpplus@gmail.com>
2011-09-13 23:52:29 +0900

    change top page where

--- app/controllers/hot_food_controller.rb
+++ app/controllers/hot_food_controller.rb
@@ -1,6 +1,6 @@
 class HotFoodController < ApplicationController
   def index
     @weekly_hot_foods = WeeklyHotFood.all
-    @recent_tweets = Tweet.order('tweets.created_at DESC').includes(:user).limit(20)
+    @recent_tweets = Tweet.where('? <= tweets.created_at', 2.weeks.ago.beginning_of_day).order('tweets.created_at DESC').includes(:user).limit(20)
   end
 end
