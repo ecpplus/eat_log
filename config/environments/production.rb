e4b13a893efa9254bd9fbc1b8e0b42337dda6e9b
chu <ecpplus@gmail.com>
2011-09-14 16:46:37 +0900

    add assets settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ EatLog::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.assets.compress = true
+  config.assets.compile = true
+  config.assets.digest = true
 end
